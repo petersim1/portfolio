ac7afb59640fe5f505229fc2e9c33cf873fd45af
Peter Simone <plssimone@gmail.com>
2023-04-29 02:25:36 -0400

    add mobile display for cycle

--- components/sections/Intro.tsx
+++ components/sections/Intro.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState, useEffect } from "react";
 import classNames from "classnames";
 
 import styles from "@/styles/layout.module.css";
@@ -6,15 +6,47 @@ import { worksans } from "@/styles/fonts";
 
 const Intro = ({ progress }: { progress: number }): JSX.Element => {
   const options = ["ml/ai", "web dev", "web3", "sports", "research"];
+  const [large, setLarge] = useState(true);
 
-  const active = useMemo((): number => {
-    return Math.min(options.length - 1, Math.round((progress / 100) * (options.length - 1)));
-  }, [progress]);
+  useEffect(() => {
+    const fixSize = (): void => {
+      const { matches } = window.matchMedia("(max-width: 850px)");
+      setLarge(!matches);
+    };
+
+    const { matches } = window.matchMedia("(max-width: 850px)");
+    setLarge(!matches);
+
+    window.addEventListener("resize", fixSize);
+
+    return () => {
+      window.addEventListener("resize", fixSize);
+    };
+  }, []);
+
+  const query = useMemo(() => {
+    const activeInd = Math.min(
+      options.length - 1,
+      Math.round((progress / 100) * (options.length - 1)),
+    );
+    let transform;
+    if (large) {
+      transform = `translateY(calc(-1*(var(--gap-vert) + 2.5rem)*${activeInd}))`;
+    } else {
+      transform = `translateX(calc(-1*(var(--fixed-width))*${activeInd}))`;
+    }
+    return {
+      active: activeInd,
+      transform,
+    };
+  }, [progress, large]);
 
   return (
     <section className="h_300" id="intro">
       <div className={styles.parallax}>
-        <div className={classNames(styles.intro_holder, worksans.className)}>
+        <div
+          className={classNames(styles.intro_holder, worksans.className, { [styles.large]: large })}
+        >
           <div>
             <h1>
               Peter
@@ -22,15 +54,18 @@ const Intro = ({ progress }: { progress: number }): JSX.Element => {
               Simone
             </h1>
           </div>
-          <div className={styles.cycle_holder}>
+          <div className={classNames(styles.cycle_holder, { [styles.large]: large })}>
             <div
-              className={styles.cycle}
-              style={{ transform: `translateY(calc(-1*(var(--gap) + 2.5rem)*${active}))` }}
+              className={classNames(styles.cycle, { [styles.large]: large })}
+              style={{ transform: query.transform }}
             >
               {options.map((option, ind) => (
                 <div
                   key={ind}
-                  className={classNames(styles.item, { [styles.active]: active === ind })}
+                  className={classNames(styles.item, {
+                    [styles.active]: query.active === ind,
+                    [styles.large]: large,
+                  })}
                 >
                   {option}
                 </div>
