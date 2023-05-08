204a6194aaa09a0be7d28bab511224bd160f10a8
Peter Simone <plssimone@gmail.com>
2023-05-07 21:44:03 -0400

    update og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,15 +6,25 @@ const Document = (): JSX.Element => {
       <Head>
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#121212" />
         <link rel="apple-touch-icon" href="/apple-touch.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="Portfolio - Peter Simone" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/og.png" />
+        <meta property="og:image" content="/og.jpg" />
         <meta property="og:description" content="Portfolio - Peter Simone" />
         <meta property="og:title" content="Portfolio" />
         <meta property="og:url" content="https://petersim.one" />
+        <meta name="twitter:image" content="/og.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@pete_sim1" />
+        <meta name="twitter:creator" content="@pete_sim1" />
+        <meta name="twitter:title" content="Portfolio - Peter Simone" />
+        <meta name="twitter:description" content="Portfolio - Peter Simone" />
+        <meta property="article:author" content="http://www.twitter.com/pete_sim1" />
       </Head>
       <body>
         <Main />
