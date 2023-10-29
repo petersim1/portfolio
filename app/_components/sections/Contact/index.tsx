b6d81ec562561c1a4bac73bd05cc240809d159c3
Peter Simone <plssimone@gmail.com>
2023-10-28 22:59:56 -0400

    update contact section

--- app/_components/sections/Contact/index.tsx
+++ app/_components/sections/Contact/index.tsx
@@ -1,46 +1,11 @@
-"use client";
-
 import classNames from "classnames";
-import { useState, useEffect } from "react";
 
-import { useScrollContext } from "@/_store/scroll";
 import styles from "./contact.module.css";
-import { worksans, spacemono, inconsolata } from "@/_lib/fonts";
+import { worksans, inconsolata } from "@/_lib/fonts";
 import { contacts } from "@/_lib/constants";
 import { Mirror, Twitter, Linkedin, Github, Mail } from "@/_lib/assets";
 
 export default (): JSX.Element => {
-  const [social, setSocial] = useState("");
-  const [prevSocial, setPrevSocial] = useState("");
-  const [typed, setTyped] = useState("");
-
-  const { progress } = useScrollContext();
-  const iProgress = progress.length > 0 ? progress[3] : 0;
-
-  useEffect(() => {
-    if (social !== prevSocial) {
-      let shrink = typed.length > 0;
-      let ind = prevSocial.length;
-      const interval = setInterval(() => {
-        if (shrink) {
-          setTyped(prevSocial.substring(0, ind));
-          ind--;
-          if (ind < 0) {
-            shrink = false;
-            ind = 1;
-          }
-        } else {
-          setTyped(social.substring(0, ind));
-          ind++;
-          if (ind > social.length) {
-            clearInterval(interval);
-          }
-        }
-      }, 20);
-      setPrevSocial(social);
-    }
-  }, [social]);
-
   const Icon = (props: React.HTMLProps<SVGElement>): JSX.Element => {
     const { type, ...rest } = props;
 
@@ -59,62 +24,30 @@ export default (): JSX.Element => {
     return <Mirror {...rest} />;
   };
 
-  const powers = [1.25, 1.2, 1.15, 1.1, 1];
-
   return (
-    <section className="h_200" id="contact" data-offset-top={0} data-offset-bottom={-10}>
+    <section className="h_100" id="contact" data-offset-top={0} data-offset-bottom={0}>
       <div className={classNames(styles.content, worksans.className)}>
-        <div className={styles.title}>
-          <h3>
-            <b>Get in Contact</b>
-          </h3>
-        </div>
-        <div className={classNames(styles.code_box, spacemono.className)}>
-          <code>
-            {`% python connect_w_me.py --method ${typed}`}
-            <span />
-          </code>
-        </div>
-        <div className={classNames(styles.contact_wrapper, inconsolata.className)}>
-          {contacts.map((contact, ind) => (
-            <a
-              key={ind}
-              href={contact.link}
-              target="_blank"
-              referrerPolicy="no-referrer"
-              onMouseEnter={(): void => setSocial(contact.type)}
-            >
-              <div className={styles.contact}>
-                <div className={styles.placeholder}>
-                  <Icon
-                    type={contact.type}
-                    height="30px"
-                    style={{ opacity: 0.1, filter: "brightness(0.5)" }}
-                  />
-                  <Icon
-                    type={contact.type}
-                    height="30px"
-                    style={{
-                      left: `calc(max(0px,100vw*(100 - ${Math.pow(iProgress, powers[ind])})/100))`,
-                      opacity: Math.min(1, Math.pow(iProgress, powers[ind]) / 200),
-                    }}
-                  />
+        <div className={styles.wrapper}>
+          <div className={styles.title}>
+            <p>
+              Feel free to <b>connect</b>
+              <br />
+              with me. I'd love to hear
+              <br />
+              what you're <b>building</b>.
+            </p>
+          </div>
+          <div className={classNames(styles.contact_wrapper, inconsolata.className)}>
+            {contacts.map((contact, ind) => (
+              <a key={ind} href={contact.link} target="_blank" referrerPolicy="no-referrer">
+                <div className={styles.contact}>
+                  <div className={styles.placeholder}>
+                    <Icon type={contact.type} height="30px" style={{ opacity: 1 }} />
+                  </div>
                 </div>
-                <div
-                  className={classNames(styles.divider, {
-                    [styles.show]: Math.min(1, Math.pow(iProgress, powers[ind]) / 100) >= 1,
-                  })}
-                />
-                <div
-                  className={classNames(styles.follow, {
-                    [styles.show]: Math.min(1, Math.pow(iProgress, powers[ind]) / 100) >= 1,
-                  })}
-                >
-                  {contact.type === "email" ? "contact" : "follow"}
-                </div>
-              </div>
-            </a>
-          ))}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </section>
