b019861f6128684c777d67ad23b9d403072bee33
Peter Simone <plssimone@gmail.com>
2023-10-30 20:41:38 -0400

    move socials to footer

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -1,17 +1,46 @@
 // import Link from "next/link";
+import classNames from "classnames";
 
 import styles from "./layout.module.css";
 import { inconsolata } from "@/_lib/fonts";
+import { contacts } from "@/_lib/constants";
+import { Mirror, Twitter, Linkedin, Github, Mail } from "@/_lib/assets";
 // import { Branch, Star, Arrow } from "@/_lib/assets";
 // import { github } from "@/_actions";
 
 export default async (): Promise<JSX.Element> => {
   // const { stars, forks } = await github();
+  const Icon = (props: React.HTMLProps<SVGElement>): JSX.Element => {
+    const { type, ...rest } = props;
+
+    if (type === "twitter") {
+      return <Twitter {...rest} fill="#1DA1F2" />;
+    }
+    if (type === "github") {
+      return <Github {...rest} fill="var(--font)" />;
+    }
+    if (type === "email") {
+      return <Mail {...rest} fill="var(--font)" />;
+    }
+    if (type === "linkedin") {
+      return <Linkedin {...rest} fill="#0072b1" />;
+    }
+    return <Mirror {...rest} />;
+  };
   return (
     <footer className={inconsolata.className}>
       <div className={styles.footer}>
         <div>
           ~ built by <b>peter simone</b> using nextjs ~
+        </div>
+        <div className={classNames(styles.contact_wrapper, inconsolata.className)}>
+          {contacts.map((contact, ind) => (
+            <a key={ind} href={contact.link} target="_blank" referrerPolicy="no-referrer">
+              <div className={styles.contact}>
+                <Icon type={contact.type} />
+              </div>
+            </a>
+          ))}
         </div>
       </div>
     </footer>
--- app/_components/sections/Contact/index.tsx
+++ app/_components/sections/Contact/index.tsx
@@ -1,29 +1,9 @@
 import classNames from "classnames";
 
 import styles from "./contact.module.css";
-import { worksans, inconsolata } from "@/_lib/fonts";
-import { contacts } from "@/_lib/constants";
-import { Mirror, Twitter, Linkedin, Github, Mail } from "@/_lib/assets";
+import { worksans } from "@/_lib/fonts";
 
 export default (): JSX.Element => {
-  const Icon = (props: React.HTMLProps<SVGElement>): JSX.Element => {
-    const { type, ...rest } = props;
-
-    if (type === "twitter") {
-      return <Twitter {...rest} fill="#1DA1F2" />;
-    }
-    if (type === "github") {
-      return <Github {...rest} fill="var(--font)" />;
-    }
-    if (type === "email") {
-      return <Mail {...rest} fill="var(--font)" />;
-    }
-    if (type === "linkedin") {
-      return <Linkedin {...rest} fill="#0072b1" />;
-    }
-    return <Mirror {...rest} />;
-  };
-
   return (
     <section className="h_100" id="contact" data-offset-top={0} data-offset-bottom={0}>
       <div className={classNames(styles.content, worksans.className)}>
@@ -32,21 +12,11 @@ export default (): JSX.Element => {
             <p>
               Feel free to <b>connect</b>
               <br />
-              with me. I'd love to hear
+              with me. <br className={styles.break_mobile} />
+              I'd love to hear
               <br />
               what you're <b>building</b>.
             </p>
-          </div>
-          <div className={classNames(styles.contact_wrapper, inconsolata.className)}>
-            {contacts.map((contact, ind) => (
-              <a key={ind} href={contact.link} target="_blank" referrerPolicy="no-referrer">
-                <div className={styles.contact}>
-                  <div className={styles.placeholder}>
-                    <Icon type={contact.type} height="30px" style={{ opacity: 1 }} />
-                  </div>
-                </div>
-              </a>
-            ))}
           </div>
         </div>
       </div>
