e89da643420a3a0750d343164b1ff6d9f647e599
Peter Simone <plssimone@gmail.com>
2023-12-19 11:11:18 -0500

    add dynamic og image per slug

--- app/(routes)/blog/[slug]/opengraph-image.tsx
+++ app/(routes)/blog/[slug]/opengraph-image.tsx
@@ -1,33 +1,17 @@
 import { ImageResponse } from "next/og";
+import { allPosts } from "contentlayer/generated";
+import OpenGraph from "@/_components/OpenGraph";
 
 // Route segment config
 export const runtime = "edge";
 
-// Image metadata
-export const alt = "About Acme";
-
 export const contentType = "image/png";
 
 export default async ({ params }: { params: { slug: string } }): Promise<ImageResponse> => {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 128,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {params.slug}
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    },
-  );
+  const post = allPosts.find((p) => p._raw.flattenedPath === params.slug);
+  const subheader = post.title || "";
+  return new ImageResponse(<OpenGraph title={"P.S. | Blog"} subheader={subheader} />, {
+    width: 1200,
+    height: 630,
+  });
 };
--- app/_components/OpenGraph/index.tsx
+++ app/_components/OpenGraph/index.tsx
@@ -1,14 +1,19 @@
+import classNames from "classnames";
 import { GeistSans } from "geist/font/sans";
+
+import styled from "./styled.module.css";
 
 export default ({ title, subheader }: { title: string; subheader: string }): JSX.Element => {
   return (
-    <div className={GeistSans.className}>
+    <div className={classNames(GeistSans.className, styled.og_wrapper)}>
       <div>
         <h1>{title}</h1>
       </div>
-      <div>
-        <p>{subheader}</p>
-      </div>
+      {subheader && (
+        <div>
+          <p>{subheader}</p>
+        </div>
+      )}
     </div>
   );
 };
