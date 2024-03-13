0332dc39b3fc1ca74c3c990ab1dab1581c542c0e
Peter Simone <plssimone@gmail.com>
2024-03-12 23:30:52 -0400

    simplify project section

--- app/_components/Projects/Project/index.tsx
+++ app/_components/Projects/Project/index.tsx
@@ -1,33 +1,31 @@
 import { Social, IconSized } from "@/_components/Icon";
-import { H3, ClippedText, Faint } from "@/_components/Text";
-import { OGImage } from "@/_components/Image";
-import { Project, Links, Tags, ImageHolder } from "./styled";
+import { H3, SubHeader, Faint } from "@/_components/Text";
+import { Project, Header, Links, Tags } from "./styled";
 import { DataI } from "@/_lib/types";
 import { Pill } from "@/_components/Common";
 
 export default ({ data }: { data: DataI }): JSX.Element => {
   return (
     <Project>
-      <H3>{data.title}</H3>
-      <ImageHolder>
-        <OGImage $url={data.description.image} />
-      </ImageHolder>
+      <Header>
+        <H3>{data.title}</H3>
+        <Links>
+          {data.description.links.map((link, ind2) => (
+            <a key={ind2} href={link.link} target="_blank" referrerPolicy="no-referrer">
+              <IconSized $size="15px">
+                <Social type={link.type} />
+              </IconSized>
+            </a>
+          ))}
+          {data.description.links.length == 0 && <Faint>{"[proprietary]"}</Faint>}
+        </Links>
+      </Header>
       <Tags>
         {data.description.tools.concat(data.description.languages).map((tool, ind2) => (
           <Pill key={ind2}>{tool}</Pill>
         ))}
       </Tags>
-      <ClippedText $lines={3}>{data.description.text}</ClippedText>
-      <Links>
-        {data.description.links.map((link, ind2) => (
-          <a key={ind2} href={link.link} target="_blank" referrerPolicy="no-referrer">
-            <IconSized $size="15px">
-              <Social type={link.type} />
-            </IconSized>
-          </a>
-        ))}
-        {data.description.links.length == 0 && <Faint>{"[proprietary]"}</Faint>}
-      </Links>
+      <SubHeader>{data.description.text}</SubHeader>
     </Project>
   );
 };
