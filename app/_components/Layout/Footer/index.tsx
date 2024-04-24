b346e51fdde1394474d5d5e9365485cd113719b8
Peter Simone <plssimone@gmail.com>
2024-04-24 11:36:31 -0400

    add projects section

--- app/_components/Layout/Footer/index.tsx
+++ app/_components/Layout/Footer/index.tsx
@@ -1,31 +1,21 @@
-import { P, Shadow } from "@/_components/Text";
 import { contacts } from "@/_lib/constants";
 
-import { Footer, FooterDiv, FooterContacts } from "./styled";
+import { Footer, FooterContacts } from "./styled";
 import { Social, IconSized } from "@/_components/Icon";
 
 export default (): JSX.Element => {
   // const { stars, forks } = await github();
   return (
     <Footer>
-      <FooterDiv>
-        <P style={{ fontSize: "0.8rem" }}>
-          ~ built by{" "}
-          <b>
-            <Shadow>peter simone</Shadow>
-          </b>{" "}
-          using nextjs ~
-        </P>
-        <FooterContacts>
-          {contacts.map((contact, ind) => (
-            <a key={ind} href={contact.link} target="_blank" referrerPolicy="no-referrer">
-              <IconSized $size="18px">
-                <Social type={contact.type} />
-              </IconSized>
-            </a>
-          ))}
-        </FooterContacts>
-      </FooterDiv>
+      <FooterContacts>
+        {contacts.map((contact, ind) => (
+          <a key={ind} href={contact.link} target="_blank" referrerPolicy="no-referrer">
+            <IconSized $size="18px">
+              <Social type={contact.type} />
+            </IconSized>
+          </a>
+        ))}
+      </FooterContacts>
     </Footer>
   );
 };
--- app/_components/Layout/Footer/styled.ts
+++ app/_components/Layout/Footer/styled.ts
@@ -2,20 +2,14 @@
 
 import styled from "styled-components";
 
-import { CommonPad, Centered, Row, Column } from "@/_components/Common";
+import { CommonPad, Centered, Row } from "@/_components/Common";
 
 export const Footer = styled.footer`
   ${CommonPad}
 `;
 
-export const FooterDiv = styled(Column)`
-  ${Centered}
-  gap: ${({ theme }): string => theme.gaps.rem05};
-  padding: ${({ theme }): string => theme.gaps.xl} 0;
-  white-space: nowrap;
-`;
-
 export const FooterContacts = styled(Row)`
   ${Centered}
+  padding: ${({ theme }): string => theme.gaps.xl} 0;
   gap: ${({ theme }): string => theme.gaps.lg};
 `;
